6ccbc527573cd84986169a45259c2f4f1668b83a
DaryaDak <82772114+DaryaDak@users.noreply.github.com>
2021-08-08 13:28:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,63 +1,61 @@
-module.exports = function toReadable(number) {
-    if (number === 0) {
-        return 'zero';
-    }
-
-    const digits = {
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine"};
-
-    const tens = {
-    1: "eleven",
-    2: "twelve",
-    3: "thirteen",
-    4: "fourteen",
-    5: "fifteen",
-    6: "sixteen",
-    7: "seventeen",
-    8: "eighteen",
-    9: "nineteen"
-    };
-   
-    const dozens = { 
-        1: "ten",
-        2: "twenty",
-        3: "thirty",
-        4: "forty",
-        5: "fifty",
-        6: "sixty",
-        7: "seventy",
-        8: "eighty",
-        9: "ninety",
-    };
-
-   
-   
-const stringNumber = number.toString();
-
-
-if (stringNumber.length === 1) {
+module.exports = function toReadable (number) {
+        if (number === 0) {
+            return 'zero';
+        }
+    
+        const digits = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine"};
+    
+        const tens = {
+        1: "eleven",
+        2: "twelve",
+        3: "thirteen",
+        4: "fourteen",
+        5: "fifteen",
+        6: "sixteen",
+        7: "seventeen",
+        8: "eighteen",
+        9: "nineteen"
+        };
+       
+        const dozens = { 
+            1: "ten",
+            2: "twenty",
+            3: "thirty",
+            4: "forty",
+            5: "fifty",
+            6: "sixty",
+            7: "seventy",
+            8: "eighty",
+            9: "ninety",
+        };
+       
+    const stringNumber = number.toString();
+    
+    
+    if (stringNumber.length === 1) {
         return digits[number];
      }
     
-     else if (stringNumber.length === 2 && stringNumber[1] === 0){  //10 20 30
+     if (stringNumber.length === 2 && stringNumber[1] === 0){  //10 20 30
          return dozens[stringNumber[0]];
      }
      else if (stringNumber.length === 2 && stringNumber[1] === 1){ //11 14 16
-         return tens[stringNumber[0]];
+         return tens[stringNumber];
      }
-     else if {
+     else {
          return `${dozens[stringNumber[0]]}${digits[stringNumber[1]]}`; //21 44 53
         }
     
-       else if (stringNumber.length === 3 && stringNumber[1] === 0 && stringNumber[2] === 0){ // 100 200 300
+        if (stringNumber.length === 3 && stringNumber[1] === 0 && stringNumber[2] === 0){ // 100 200 300
             return `${digits[stringNumber[0]]} hundred`;
         }
         else if (stringNumber.length === 3 && stringNumber[1] === 0 && stringNumber[2] > 0){ // 102 304 507
@@ -73,4 +71,4 @@ if (stringNumber.length === 1) {
     else {
         return `${digits[stringNumber[0]]} hundred ${tens[stringNumber[1]]} ${digits[stringNumber[2]]}`;
     }
-};
+    };
