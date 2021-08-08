ad97e7b371e2d556b2d78ffe1a60578ceae6ca7f
DaryaDak <82772114+DaryaDak@users.noreply.github.com>
2021-08-08 11:39:58 +0300

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -3,22 +3,32 @@ module.exports = function toReadable(number) {
         return 'zero';
     }
 
-    let digits = "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
-    let tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    let digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    let tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
    
 const stringNumber = number.toString();
 const dec = tens[stringNumber[0]];
-const hundreds = digits[stringNumber[0]];
-const units = digits[stringNumber[1]]; 
+const units = digits[stringNumber[1]];
 
-if (number > 0 && number < 20) {
+
+if (number < 20) {
     return digits[number];
  }
 
- else if (number >= 20 && number < 100){
-     return `${dec} ${units}`;
+else if (number >= 20 && number < 100 && stringNumber[1] === 0){
+     return tens[stringNumber[0]];
  }
- else if (stringNumber.length === 3){
-    return `${hundreds} ${dec} ${units}`;
+
+ else if (number >= 20 && number < 100){
+     return dec +' '+units;
+ }
+if (stringNumber.length === 3 && stringNumber[1] === 0 && stringNumber[2] === 0){
+return (digits[stringNumber[0]] + ' hundred');
+}
+  else if (stringNumber.length === 3 && stringNumber[1] >= 1 && stringNumber[2] === 0){
+      return (digits[stringNumber[0]] + ' hundred'+ tens[stringNumber[0]] +' dozen');
+  }
+  else if (stringNumber.length === 3 && stringNumber[1] >= 1 && stringNumber[2] >= 1){
+      return (digits[stringNumber[0]] + ' hundred' + tens[stringNumber[1]] +' dozen' + digits[stringNumber[2]]);
  }
 };
