c142e02eb5ec67dc1efaec1f806851f5cadb4634
DaryaDak <82772114+DaryaDak@users.noreply.github.com>
2021-08-08 11:44:47 +0300

    fix fix code

--- src/index.js
+++ src/index.js
@@ -3,8 +3,38 @@ module.exports = function toReadable(number) {
         return 'zero';
     }
 
-    let digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-    let tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const digits = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15:  "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen"
+    };
+
+    const tens = {
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy"
+    80: "eighty"
+    90: "ninety"
+    };
    
 const stringNumber = number.toString();
 const dec = tens[stringNumber[0]];
