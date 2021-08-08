adfb2e02d559e867055396cc6a39256bd63dee54
DaryaDak <82772114+DaryaDak@users.noreply.github.com>
2021-08-08 13:38:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ module.exports = function toReadable (number) {
          return dozens[stringNumber[0]];
      }
      else if (stringNumber.length === '00' && stringNumber[0] === '1' && stringNumber[0] < '2'){ //11 14 16
-         return tens[stringNumber[0];
+         return tens[stringNumber];
      }
     
     };
