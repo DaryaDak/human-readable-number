4511de283ef3502deefb2685fe2cae67f8203b93
DaryaDak <82772114+DaryaDak@users.noreply.github.com>
2021-08-08 14:06:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -51,6 +51,6 @@ module.exports = function toReadable (number) {
          return `${dozens[stringNumber[0]]} ${digits[stringNumber[1]]}`
      }
     else if (number >= 100 && stringNumber[1] === 0 && stringNumber[2] === 0){
-        return `${digits[stringNumber[0]]} 'hundred'`;
+        return `${digits[stringNumber[0]]} hundred`;
     }
     };
